41236a6586510ed8c42e1126bd967bddb5e7c29d
nehabankar18 <01fe22bca040@kletech.ac.in>
2024-01-24 13:21:35 +0530

    fibonacci series

---
+++ fibb.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class fibb {
+    int fib(int n){
+        if(n<=1){
+          return n;
+        }else{
+            return fib(n-1)+fib(n-2);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+         System.out.print("Enter the number for fibonacci series : ");
+         n = sc.nextInt();
+         fibb obj=new fibb();
+         System.out.print("The Fibonacci series are : ");
+         for(int i=0;i<n;i++){
+           System.out.print(obj.fib(i)+" ");}
+    }
+}
+
